e40233c7580c1d705c8574abef8a579d5140945f
Fangzheng Liu <fzliu@mit.edu>
2024-01-04 00:08:26 -0500

    udated by FZ

--- Firmware/Canary_SAMD21_V1/src/main.cpp
+++ Firmware/Canary_SAMD21_V1/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-#include <RTCZero.h>
+
+#include "RTC_FZ.h"
 
 #if !( defined(ARDUINO_SAMD_ZERO) || defined(__SAMD21G18A__))
   #error This code is designed to run on SAMD21/SAMD51 platform! Please check your Tools->Board setting.
@@ -16,12 +17,10 @@ void setup() {
   // https://github.com/IowaDave/SAMD21-RTC-Clock
   // https://blog.thea.codes/understanding-the-sam-d21-clocks/
 
-  rtc.begin(true, 1);                           // initialize RTC: reset starting value, Mode 1 (16-bit counter)
-  // rtc.enableCounter(1);                    // set counter compare values (interrupt at 10 and 30)
-  rtc.setPeriod(5);                            // set counter period
-  rtc.enableOverflow();                         // enable interrupt on overflow
-  rtc.attachInterrupt(alarmMatch);                // attach interrupt
-
+  rtc.begin(true, 1); // initialize RTC: reset starting value, Mode 1 (16-bit counter)
+  rtc.setPeriod(2); // set counter period
+  rtc.enableOverflow(); // enable interrupt on overflow
+  rtc.attachInterrupt(alarmMatch); // attach interrupt
 }
 
 void loop() {
