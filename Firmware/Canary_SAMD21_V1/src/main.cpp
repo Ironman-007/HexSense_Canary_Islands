66c6cbef36ee82a0d608d28c7f1feb344daebd85
Fangzheng Liu <fzliu@mit.edu>
2024-01-10 18:32:27 -0500

    udated by FZ

--- Firmware/Canary_SAMD21_V1/src/main.cpp
+++ Firmware/Canary_SAMD21_V1/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+// #include "LowPower.h"
 #include "RTC_FZ.h"
 
 #if !( defined(ARDUINO_SAMD_ZERO) || defined(__SAMD21G18A__))
@@ -10,6 +11,15 @@
 
 RTCZero rtc;
 
+enum low_power_t {
+  LOW_POWER_IDLE0,
+  LOW_POWER_IDLE1,
+  LOW_POWER_IDLE2,
+  LOW_POWER_STANDBY
+};
+
+#define LOW_POWER_LEVEL LOW_POWER_STANDBY
+
 volatile bool alarmMatched = false;
 
 void alarmMatch(void) {
@@ -18,17 +28,23 @@ void alarmMatch(void) {
 
 void blink_led(void) {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
+  delay(500);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
 void set_idle_level(int level) {
-  SCB->SCR &= ~SCB_SCR_SLEEPDEEP_Msk; // Clear SLEEPDEEP bit, we use the idle state.
+  if (level < 3) {
+    SCB->SCR &= ~SCB_SCR_SLEEPDEEP_Msk; // Clear SLEEPDEEP bit, we use the idle state.
 
-  if (level == 0) {PM->SLEEP.reg = PM_SLEEP_IDLE_CPU;}
-  else if (level == 1) {PM->SLEEP.reg = PM_SLEEP_IDLE_AHB;}
-  else if (level == 2) {PM->SLEEP.reg = PM_SLEEP_IDLE_APB;}
-  else {PM->SLEEP.reg = PM_SLEEP_IDLE_CPU;}
+    if (level == 0) {PM->SLEEP.reg = PM_SLEEP_IDLE_CPU;}
+    if (level == 1) {PM->SLEEP.reg = PM_SLEEP_IDLE_AHB;}
+    if (level == 2) {PM->SLEEP.reg = PM_SLEEP_IDLE_APB;}
+  }
+  else {
+    SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+  }
+  __DSB();
+  __WFI();
 }
 
 void setup() {
@@ -39,22 +55,18 @@ void setup() {
   // https://blog.thea.codes/understanding-the-sam-d21-clocks/
 
   rtc.begin(true, 1);              // initialize RTC: reset starting value, Mode 1 (16-bit counter)
-  rtc.setPeriod(PERIOD-1);                // set counter period
+  rtc.setPeriod(PERIOD-1);         // set counter period
   rtc.enableOverflow();            // enable interrupt on overflow
-  rtc.attachInterrupt(alarmMatch); // attach interrupt
-
-  set_idle_level(0);
+  rtc.attachInterrupt(alarmMatch); // attach interrupt handler
 }
 
 void loop() {
-  __DSB();
-  __WFI();
+  set_idle_level(LOW_POWER_LEVEL);
 
   if (alarmMatched) {
     alarmMatched = false;
     blink_led();
+    set_idle_level(2);
   }
 }
-
-
 
