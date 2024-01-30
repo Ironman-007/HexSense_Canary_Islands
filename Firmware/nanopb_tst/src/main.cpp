10ed63963a1387e3cc245aec27c8f879f32058e6
Fangzheng Liu <fzliu@mit.edu>
2024-01-29 23:01:55 -0500

    udated by FZ

--- Firmware/nanopb_tst/src/main.cpp
+++ Firmware/nanopb_tst/src/main.cpp
@@ -11,6 +11,9 @@ bool    status;
 
 float step = 0.0;
 
+uint32_t seq_num_i = 0;
+uint32_t crc_i     = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -20,7 +23,6 @@ void setup() {
 }
 
 void decodeReceivedData(const uint8_t* data, size_t size) {
-  /* Allocate space for the decoded message. */
   Env_data_frame env_data_frame_recv  = Env_data_frame_init_zero;
   pb_istream_t   stream_recv          = pb_istream_from_buffer(data, size);
 
@@ -29,32 +31,49 @@ void decodeReceivedData(const uint8_t* data, size_t size) {
     Serial.print(" ");
   }
   Serial.println("");
-
-  /* Now we are ready to decode the message. */
   bool status = pb_decode(&stream_recv, Env_data_frame_fields, &env_data_frame_recv);
 
-  /* Check for errors... */
   if (!status){
     Serial.println("Decoding failed");
   }
 
   /* Print the data contained in the message. */
+  Serial.print("ID: ");
+  Serial.println(env_data_frame_recv.ID);
+  Serial.print("time_stamp: ");
+  Serial.println(env_data_frame_recv.time_stamp);
+  Serial.print("seq_num: ");
+  Serial.println(env_data_frame_recv.seq_num);
   Serial.print("temperature: ");
   Serial.println(env_data_frame_recv.temperature);
   Serial.print("humidity: ");
   Serial.println(env_data_frame_recv.humidity);
   Serial.print("co2_level: ");
   Serial.println(env_data_frame_recv.co2_level);
+  Serial.print("CRC: ");
+  Serial.println(env_data_frame_recv.crc);
+}
+
+void send2LoRa(uint8_t* buffer, size_t message_length) {
+  ;
+}
+
+void cobs_encode_msg(uint8_t* buffer, size_t message_length) {
+  ;
 }
 
 void loop() {
   Env_data_frame env_data_frame = Env_data_frame_init_zero;
   pb_ostream_t   stream         = pb_ostream_from_buffer(buffer, sizeof(buffer));
 
-  // put your main code here, to run repeatedly:
-  env_data_frame.temperature     = 25.0   + step;
-  env_data_frame.humidity        = 50.0   + step;
-  env_data_frame.co2_level       = 1000.0 + step;
+  env_data_frame.ID          = 10;
+  env_data_frame.time_stamp  = millis();
+  env_data_frame.seq_num = seq_num_i;
+  env_data_frame.temperature = 25.0   + step;
+  env_data_frame.humidity    = 50.0   + step;
+  env_data_frame.co2_level   = 1000.0 + step;
+  crc_i = env_data_frame.time_stamp + env_data_frame.seq_num + env_data_frame.temperature + env_data_frame.humidity + env_data_frame.co2_level;
+  env_data_frame.crc         = crc_i;
 
   status = pb_encode(&stream, Env_data_frame_fields, &env_data_frame);
   message_length = stream.bytes_written;
@@ -64,8 +83,9 @@ void loop() {
   Serial.println(message_length);
 
   step += 0.1;
+  seq_num_i++;
 
-  decodeReceivedData(buffer, message_length);
+  // decodeReceivedData(buffer, message_length);
 
-  delay(1000);
+  delay(2000);
 }
